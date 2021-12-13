34477179f51661ff47ff736836582af35e5a9ee1
alisav <alisav@hotmail.com.tr>
2021-12-13 20:20:21 +0300

    Add project files.

---
+++ ESourcing.Sourcing/Mapping/SourcingMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESourcing.Sourcing.Mapping
+{
+    public class SourcingMapping: Profile
+    {
+        public SourcingMapping()
+        {
+            
+        }
+    }
+}
