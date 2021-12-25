c516c88abf6f13f31e72a4cbe2be1e61820756d6
alisav <alisav@hotmail.com.tr>
2021-12-25 16:55:54 +0300

    Order consumer added.

---
+++ ESourcing.Order/Extensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,34 @@
+﻿using ESourcing.Order.Consumers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ESourcing.Order.Extensions
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static EventBusOrderCreateConsumer Listener { get; set; }
+
+        public static IApplicationBuilder UserRabbitListener(this IApplicationBuilder app)
+        {
+            Listener = app.ApplicationServices.GetService<EventBusOrderCreateConsumer>();
+            var life = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            life.ApplicationStarted.Register(OnStarted);
+            life.ApplicationStopping.Register(OnStopping);
+
+            return app;
+        }
+
+        private static void OnStopping()
+        {
+            Listener.Consume();
+        }
+
+        private static void OnStarted()
+        {
+            Listener.Disconnect();
+        }
+    }
+}
--- ESourcing.Order/Startup.cs
+++ ESourcing.Order/Startup.cs
@@ -1,3 +1,5 @@
+using ESourcing.Order.Consumers;
+using EventBusRabbitMQ;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +7,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +32,48 @@ namespace ESourcing.Order
         {
 
             services.AddControllers();
+            services.AddInfrastructure(Configuration);
+            services.AddApplication();
+            services.AddAutoMapper(typeof(Startup));
+
+            #region EventBus
+
+            services.AddSingleton<IRabbitMQPersistentConnection>(sp => {
+                var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = Configuration["EventBus:HostName"]
+                };
+
+                if (!string.IsNullOrWhiteSpace(Configuration["EventBus:UserName"]))
+                {
+                    factory.UserName = Configuration["EventBus:UserName"];
+                }
+
+                if (!string.IsNullOrWhiteSpace(Configuration["EventBus:Password"]))
+                {
+                    factory.UserName = Configuration["EventBus:Password"];
+                }
+
+                var retryCount = 5;
+                if (!string.IsNullOrWhiteSpace(Configuration["EventBus:RetryCount"]))
+                {
+                    retryCount = int.Parse(Configuration["EventBus:RetryCount"]);
+                }
+
+                return new DefaultRabbitMQPersistentConnection(factory, retryCount, logger);
+            });
+
+            services.AddSingleton<EventBusOrderCreateConsumer>();
+
+            #endregion
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title="Order API", Version = "v1"});
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +91,12 @@ namespace ESourcing.Order
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v11/swagger.json", "Order API V1");
             });
         }
     }
